561f3a5ea23f1ef44d68bff2ba55a8d846b7cc3d
muahand-neyma <losylos084@gamil.com>
2024-04-03 03:02:22 -0700

    connect mot mongoDb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express' 
+import mongoose from "mongoose"
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express() 
+
+mongoose.connect(process.env.MONG_DB)
+.then(() => console.log("Db Connected"))
+.catch((error) => console.log(error))
+
+
+
 
 app.listen(5000,()=>{
     console.log("Server is ruing")
