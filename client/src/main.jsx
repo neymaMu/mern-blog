c1bcb170d0b051e836274a9da46005bbbd030016
muahand-neyma <losylos084@gamil.com>
2024-04-05 22:44:06 -0700

    prisit toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,16 +4,18 @@ import App from './App.jsx'
 import './index.css'
 
 import {Provider} from 'react-redux'
-import { store } from './redux/store.js'
-
+import { store,persistor } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    
-   <Provider store={store}>
+<PersistGate persistor={persistor}>
+<Provider store={store}>
   
     <App />
 
 
       </Provider>
+      </PersistGate>
 )
