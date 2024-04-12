66eed43b7c25eab1c525cb9f6b605cc5af3a5735
muahand-neyma <losylos084@gamil.com>
2024-04-12 04:29:38 -0700

    complete upload image

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,12 +1,76 @@
-import React from 'react'
-import{Button, FileInput, Select, TextInput} from 'flowbite-react'
+import React, { useState } from 'react'
+import{Alert, Button, FileInput, Select, TextInput} from 'flowbite-react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';  
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firbase';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+
+     
 
 
 
-   export default function CreatePost() {
-  return (
+    export default function CreatePost() {
+    
+    const[file,setFile] = useState(null)
+   
+    const[imageProgress,setImageProgress] = useState(null)
+   
+    const[imageError,setImageError] = useState(null)
+    
+    const[formData,setFormData] = useState({})
+   
+   
+   
+   
+    const handleuploadimage = async() =>{
+
+       try{
+
+         if(!file){
+          setImageError('please select an image')
+       
+          return
+      }
+       setImageError(null)
+      const storage = getStorage(app)
+      const fileName = new Date().getTime() + '-' + file.name
+      
+      const storageRef = ref(storage,fileName)
+      const uploadTask = uploadBytesResumable(storageRef,file)
+      
+      uploadTask.on(
+        'state_changed',
+        (snapshot)=>{
+          const progress = 
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          setImageProgress(progress.toFixed(0))
+        },
+
+        (error)=>{
+          setImageError('image upload faild')
+          setImageProgress(null)
+        },
+        ()=>{
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>{
+            setImageProgress(null)
+            setImageError(null)
+            setFormData({ ...formData,image:downloadURL})
+          })
+        }
+       )
+    }
+        catch(error){
+        console.log(error)
+       }
+     }
+    
+    
+    
+    
+    return (
     
     
     <div className='p-3  max-w-3xl  mx-auto min-h-screen'>
@@ -36,18 +100,38 @@ import 'react-quill/dist/quill.snow.css';
          
            <div className='flex gap-4 justify-between items-center border-1 border-teal-500 border-dotted p-3'>
 
-           <FileInput type="file" accept='image/*' />
-           <Button type="button" gradientDuoTone="purpleToBlue" size="sm" outline>upload image</Button>
+           <FileInput type="file" accept='image/*'  onChange={(e) => setFile(e.target.files[0])}/>
+           
+           <Button disabled={imageProgress} onClick={handleuploadimage} type="button" gradientDuoTone="purpleToBlue" size="sm" outline>
+           
+           {
+            imageProgress ? 
+            <div className='w-16 h-16'>
+              <CircularProgressbar value={imageProgress} text={`${imageProgress || 0}%`}/>
+            </div>
+           
+           :'upload Image'}
+         
+         
+           </Button>
 
            </div>
          
          
+         {imageError && <Alert color="failure">{imageError}</Alert>}
          
-         
+           
+           {formData.image && <img src={formData.image} alt='upload' className='w-full h-72 object-cover'/> }
+           
+           
            <ReactQuill required placeholder='Write Somthing' className='h-72 mb-12'/>
            
            
-           <Button type="submit" gradientDuoTone="purpleToPink">Publish</Button>
+           <Button type="submit" gradientDuoTone="purpleToPink">Publish
+           
+           
+           
+           </Button>
            
             </form>
        
