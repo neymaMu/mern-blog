e9d0a05b21d0ed73b9043cf69ae35b4b06cdab37
muahand-neyma <losylos084@gamil.com>
2024-04-17 13:16:44 -0700

    try to

--- api/index.js
+++ api/index.js
@@ -15,12 +15,10 @@ dotenv.config()
 const app = express() 
 
 app.use(express.json())
-app.use(cors({credentials:true}))
+app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-  }) 
+
 
 
 mongoose.connect(process.env.MONG_DB)
