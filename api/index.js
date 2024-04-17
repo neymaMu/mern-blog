9bc6766d4bb75e29310a0d8cda2bf80d96dd5a4d
muahand-neyma <losylos084@gamil.com>
2024-04-17 14:43:15 -0700

    change headers orgin

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://example.com");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
