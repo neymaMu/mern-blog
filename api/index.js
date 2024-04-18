77c027affd79a21920e5bf962de43a680b8e2cee
muahand-neyma <losylos084@gamil.com>
2024-04-18 07:33:32 -0700

    bestcoment

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({origin:["http://localhost:5173","https://mern-blog-kdbu.onrender.com"],credentials:true}))
-app.use(cookieParser());
+app.use(cookieParser())
 
 const origin =
   process.env.NODE_ENV === "production"
