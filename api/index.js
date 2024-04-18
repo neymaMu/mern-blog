623c27c485c179816c8e5874faf145bce32182c9
muahand-neyma <losylos084@gamil.com>
2024-04-18 00:51:22 -0700

    new change

--- api/index.js
+++ api/index.js
@@ -34,14 +34,10 @@ app.use(cookieParser());
 
  app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(200);
-    }
     next();
   });
 
