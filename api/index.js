4802f65afe53340e358ad16b80bfe5297342359c
muahand-neyma <losylos084@gamil.com>
2024-04-18 00:39:27 -0700

    change method

--- api/index.js
+++ api/index.js
@@ -21,18 +21,29 @@ app.use(cookieParser());
 
 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", req.header('Origin'));
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
+//app.use(function(req, res, next) {
+    //res.header("Access-Control-Allow-Origin", req.header('Origin'));
+    //res.header("Access-Control-Allow-Credentials", true);
+   // res.header(
+      //"Access-Control-Allow-Headers",
+      //"Origin, X-Requested-With, Content-Type, Accept"
+    //);
+    //res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    //next();
+ // });
+
+ app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
     );
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
     next();
   });
-
-
 
  
 
