45b1ec0204e11d906f765c03f47540cb019b51b3
muahand-neyma <losylos084@gamil.com>
2024-04-18 02:09:23 -0700

    add server

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ dotenv.config()
 const app = express() 
 
 app.use(express.json())
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:["http://localhost:5173","https://mern-blog-kdbu.onrender.com"],credentials:true}))
 app.use(cookieParser());
 
 
